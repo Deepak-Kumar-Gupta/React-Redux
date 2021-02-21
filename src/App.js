e6a162dfb603df9918e24449074e4b177b3982c6
Deepak Kumar <dkm6993@gmail.com>
2021-02-21 12:53:19 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './App.css';
-import CakeContainer from './components/CakeContainer'
 import store from './redux/store'
 import { Provider } from 'react-redux'
+import HooksCakeContainer from './components/HooksCakeContainer';
+import CakeContainer from './components/CakeContainer'
 
 
 function App() {
   return (
     <Provider store={store} >
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
