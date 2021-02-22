55fa6f04fb070a0b71fb15331f5b62a6125e945b
Deepak Kumar <dkm6993@gmail.com>
2021-02-22 10:31:09 +0530

    .

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -2,12 +2,26 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../redux'
 
-function UserContainer() {
-    return (
-        <div>
-            users
-        </div>
-    )
+function UsersContainer({ userData, fetchUsers }) {
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+    return userData.loading ? (
+        <h2>loading</h2>
+    ) : userData.error ? (
+        <h2>{userData.error}</h2>
+    ) : (
+                <div>
+                    <h2>User List</h2>
+                    <div>
+                        {
+                            userData &&
+                            userData.users &&
+                            userData.users.map(user => <p>{user.name}</p>)
+                        }
+                    </div>
+                </div>
+            )
 }
 
 const mapStateToProps = state => {
@@ -27,4 +41,3 @@ export default connect(
     mapDispatchToProps
 )(UsersContainer)
 
-export default UserContainer
